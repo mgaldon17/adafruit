7e4b9bb6a8c3462fe3419fdd5cbe1ae84e14c957
Manuel Galdon <mgaldon49@gmail.com>
2020-01-11 18:21:22 +0100

    edit script

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -28,7 +28,7 @@ sensor = Adafruit_DHT.DHT22
 
 # Example using a Beaglebone Black with DHT sensor
 # connected to pin P8_11.
-pin = 'P8_11'
+pin = 4
 
 # Example using a Raspberry Pi with DHT sensor
 # connected to GPIO23.
@@ -46,3 +46,7 @@ if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
+    
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
